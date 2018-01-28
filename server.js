9c169c9971b32adeace032b946c14e2ac8aaa9df
Bill <iamkg05@gmail.com>
2018-01-28 01:07:08 -0600

    can return variable with post request

--- server.js
+++ server.js
@@ -1,21 +1,54 @@
-const express = require('express'); const app = express(); const fs = require('fs'); const allowCrossDomain = function(req, res, next) { res.header('Access-Control-Allow-Origin', '*'); res.header('Access-Control-Allow-Methods', 'GET'); res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const allowCrossDomain = function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
 }
 
+const readFile = function(req, res, next){
+  const file = fs.readFileSync("./assets/variable-dict.txt");
+  fileObj = JSON.parse(file);
+  next();
+}
+
+let fileObj = null;
 app.use(allowCrossDomain);
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+app.use(readFile);
 
 app.get('/api/getProjectNames', (req, res) => {
-  //res.header('Content-Type', 'application/json');
   projectNames = getProjectName();
-  res.send(projectNames)
+  res.send(projectNames);
 });
 
+app.post('/api/getProjectVariables', (req, res) => {
+  res.header('Content-Type', 'application/json');
+  let selectedProjects = req.body.selectedProjects;
+  let variablesObj = getProjectVariables(selectedProjects);
+  res.json(variablesObj);
+});
+
+function getProjectVariables(names){
+  projectVarObj = {};
+  names.map(name => {
+    name = name.charAt(0).toLowerCase() + name.slice(1);
+    projectVarObj[name] = fileObj[name];
+  });
+  return projectVarObj;
+}
+
 function getProjectName(){
-  const file = fs.readFileSync("./assets/variable-dict.txt");
+  //const file = fs.readFileSync("./assets/variable-dict.txt");
   //console.log(JSON.parse(file));
-  fileObj = JSON.parse(file);
+  //fileObj = JSON.parse(file);
   const projectNames = Object.keys(fileObj).map(name=>{
-    return name.replace('-','/');
+    return name.charAt(0).toUpperCase() + name.slice(1);
   });
   return projectNames;
 }
